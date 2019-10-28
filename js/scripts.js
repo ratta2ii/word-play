89c1090e662f6f0d8d70e9280aa506bcc470d83b
Student <example@example.com>
2019-10-28 15:21:10 -0700

    Begin word play project.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,18 @@
 $(document).ready(function() {
 
+  $("#form1").submit(function(event) {
+    event.preventDefault();
 
+    var formInput = $("#input-field").val();
+    var words = formInput.split(" ");
 
+    let finalArray = words.filter(function(word) {
+      return word.length >= 3;
+    });
+
+    var finalSentence = finalArray.reverse().join(" ");
+
+    $("#results-para").text(finalSentence);
+    $(".results").show();
+ });
 });
